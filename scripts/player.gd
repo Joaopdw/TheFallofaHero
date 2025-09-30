11017648f9254f2f496ac88069b5c5619f3a0183
Joaopdw <joaoarthurpdw@gmail.com>
2025-09-30 14:41:25 -0300

    Atualização de Menu e Organização

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 50.0
 
-@onready var animation := $AnimatedSprite2D as AnimatedSprite2D
+@onready var animation := $Sprite2D as AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
@@ -11,19 +11,19 @@ func _physics_process(delta: float) -> void:
 	var directionx := Input.get_axis("ui_left", "ui_right")
 	if directionx:
 		velocity.x = directionx * SPEED
-		animation.play("FeliciadeLado")
+		animation.play("walkRight")
 		animation.scale.x = directionx
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animation.play("FeliciaParada")
+		animation.play("default")
 		
 	var directiony := Input.get_axis("ui_up", "ui_down")
 	if directiony:
 		velocity.y = directiony * SPEED
 		if directiony == 1:
-			animation.play("FeliciaFrente")
-		if directiony == -1:
-			animation.play("FeliciaCostas")
+			animation.play("walkDown")
+		elif directiony == -1:
+			animation.play("walkUp")
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 
